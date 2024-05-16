8dcb5ac645eda516d7ad33824022a74921f110c1
quyn2904 <quyenpq.work@gmail.com>
2024-05-17 04:39:07 +0700

    feature: Login UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import { NextUIProvider } from "@nextui-org/react";
+import { lazy } from "react";
+import "@styles/index.scss";
+import { Route, Routes } from "react-router-dom";
+import ThemeStyles from "@styles/theme";
+import { ThemeProvider } from "styled-components";
+import { useTheme } from "@contexts/themeContext";
+
+const Login = lazy(() => import("@pages/Login"));
 
 const App = () => {
+  const { theme } = useTheme();
   return (
     <>
       <NextUIProvider>
-        <h1 className="text-red-400">Hello World</h1>
+        <ThemeProvider theme={{ theme: theme }}>
+          <ThemeStyles />
+          <Routes>
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </ThemeProvider>
       </NextUIProvider>
     </>
   );
